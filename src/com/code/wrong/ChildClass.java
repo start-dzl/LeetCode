44e7ea1803cc38c478d036049655556abfddcdad
dengzuliang <1243804796@qq.com>
2022-03-26 21:45:30 +0800

    add 第21天

--- src/com/code/wrong/ChildClass.java
+++ src/com/code/wrong/ChildClass.java
@@ -2,12 +2,15 @@ package com.code.wrong;
 
 public class ChildClass extends ParentClass{
     private int childX = 1;
-    public ChildClass() {}
+    public ChildClass(
+    ) {
+        System.out.println("ChildX 被赋值为 " + this.childX);
+    }
     @Override
     public void setX(int x) {
         super.setX(x);
         this.childX = x;
-        System.out.println("ChildX 被赋值为 " + x);
+        System.out.println("ChildX 被赋值为 " + this.childX);
     }
     public void printX() {
         System.out.println("ChildX = " + childX);
