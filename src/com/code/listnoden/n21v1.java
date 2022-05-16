a6fef436bea2327b3ac4f52c9d151e30998cb5f4
dengzuliang <1243804796@qq.com>
2022-05-16 22:54:04 +0800

    add 第21天

---
+++ src/com/code/listnoden/n21v1.java
@@ -0,0 +1,56 @@
+package com.code.listnoden;
+
+public class n21v1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode next = head;
+        while (list1 != null || list2 != null){
+            if(list1 == null) {
+                next.next = list2;
+                next = next.next;
+                break;
+            }
+
+            if(list2 == null) {
+                next.next = list1;
+                next = next.next;
+                break;
+            }
+
+            if(list1.val >= list2.val) {
+                next.next = list2;
+                next = next.next;
+                list2 = list2.next;
+            }else {
+                next.next = list1;
+                next = next.next;
+                list1 = list1.next;
+            }
+
+        }
+        return head.next;
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
