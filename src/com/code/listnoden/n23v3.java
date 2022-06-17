e58ec62001b6d50b796f6edfea08559e2cb3f680
dengzuliang <1243804796@qq.com>
2022-06-17 21:19:44 +0800

    add 算法学习

--- src/com/code/listnoden/n23v3.java
+++ src/com/code/listnoden/n23v3.java
@@ -1,4 +1,47 @@
 package com.code.listnoden;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
 public class n23v3 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(Objects.isNull(lists) || lists.length==0) {
+            return null;
+        }
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(
+                (s1,s2)-> {
+                    return s1.val - s2.val;
+                }
+        );
+        for (ListNode node : lists) {
+            if(!Objects.isNull(node)) {
+                priorityQueue.add(node);
+            }
+        }
+
+        ListNode dump = new ListNode();
+        ListNode node = dump;
+
+        while (!priorityQueue.isEmpty()){
+            ListNode poll = priorityQueue.poll();
+            if(!Objects.isNull(poll.next)) {
+                priorityQueue.add(poll.next);
+            }
+            node.next = poll;
+            node = node.next;
+        }
+
+        return dump.next;
+
+    }
+
+     public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
 }
