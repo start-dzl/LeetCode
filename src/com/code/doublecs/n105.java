e04f3e1dae05863d323973fb0399cf958d7e31db
dengzuliang <1243804796@qq.com>
2022-08-22 22:22:11 +0800

    add 二叉树思路

--- src/com/code/doublecs/n105.java
+++ src/com/code/doublecs/n105.java
@@ -2,9 +2,38 @@ package com.code.doublecs;
 
 import com.code.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class n105 {
 
-//    public TreeNode buildTree(int[] preorder, int[] inorder) {
-//
-//    }
+    private Map<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+       return buildTree(preorder, inorder, 0, preorder.length-1,0, inorder.length );
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder,
+                              Integer preStart, Integer preEnd,
+                              Integer inStart, Integer inEnd) {
+
+        if (preStart > preEnd) {
+            return null;
+        }
+        int i = preorder[preStart];
+        TreeNode treeNode = new TreeNode(i);
+        Integer inorderIndex = map.get(i);
+        //找到对应值的位置
+        int leftNum = inorderIndex - inStart;
+        //left
+        treeNode.left = buildTree(preorder, inorder, preStart + 1, preStart + leftNum, inStart, inorderIndex - 1);
+        //right
+
+        treeNode.right = buildTree(preorder, inorder, preStart + leftNum + 1, preEnd, inorderIndex + 1, inEnd);
+
+        return treeNode;
+    }
 }
