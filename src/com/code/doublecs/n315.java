4e7661cec0459f4e61291196dba7e917ffdac135
dengzuliang <1243804796@qq.com>
2022-10-06 15:32:54 +0800

    add 归并排序

---
+++ src/com/code/doublecs/n315.java
@@ -0,0 +1,52 @@
+package com.code.doublecs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class n315 {
+    int[] temp;
+
+//    public List<Integer> countSmaller(int[] nums) {
+//
+//
+//    }
+
+    private void sort(int[] nums, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+
+        int mid = start + (end-start)/2;
+
+        sort(nums, start, mid);
+
+        sort(nums, mid+1, end);
+
+        marge(nums, start, end, mid);
+
+    }
+
+    private void marge(int[] nums, int start, int end, int mid) {
+
+        for (int i = start; i <=end; i++) {
+            temp[i] = nums[i];
+        }
+        int s = start ;
+        int m = mid +1;
+        for (int i = start; i <= end ; i++) {
+            if(s == (mid+1)) {
+                nums[i] = temp[m++];
+            }else if(m == (end+1)) {
+                nums[i] = temp[s++];
+            }
+            else if(temp[s] < temp[m]) {
+                nums[i] = temp[s++];
+            }else {
+                nums[i] = temp[m++];
+            }
+
+        }
+
+    }
+
+}
